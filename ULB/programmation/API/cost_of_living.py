352d33b8ba5e99f2ef4058cb92d606916d097e1b
Bjorn Silvester <bjorn.silvester@gmail.com>
2026-09-17 07:58:46 +0200

    feat: starting new project for learning APIs

---
+++ ULB/programmation/API/cost_of_living.py
@@ -0,0 +1,21 @@
+salary = float(input("Average monthly salary : "))
+rent = float(input("Average monthly rent : "))
+cost_monthly = float(
+    input("Average amount you spend on shopping and utility monthly : ")
+)
+
+
+def cost_of_living(salary, rent, cost_monthly):
+    cash_after_rent = salary - rent
+    net_savings = cash_after_rent - cost_monthly
+
+    print(
+        f"Average monthly salary = {salary},\nAverage monthly rent = {rent},\nAverage monthly cash after rent = {cash_after_rent},\nAverage monthly cost of living = {cost_monthly},\nNet savings after everything = {net_savings}"
+    )
+
+    return 0
+
+def compare();
+
+
+cost_of_living(salary, rent, cost_monthly)
